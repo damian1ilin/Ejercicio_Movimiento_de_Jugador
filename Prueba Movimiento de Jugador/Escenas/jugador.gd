a27246cb3d20a845ab6ede0b175d1aca9d395119
damian1ilin <damian.franco.ilincheta@gmail.com>
2024-04-12 11:10:12 -0300

    Puesta en escena con el "bug"/problema

--- Prueba Movimiento de Jugador/Escenas/jugador.gd
+++ Prueba Movimiento de Jugador/Escenas/jugador.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
+@export var max_speed : int = 500
+@export var gravity : float = 2000
+@export var jump_force : int = 500
+@export var acceleration : int = 100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
+func _physics_process(_delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * _delta
+		#if velocity.y > 2000:
+			#velocity.y = 2000
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("ui_right"):
+		if (velocity.x < 0):
+			velocity.x = velocity.x * -1
+		velocity.x += acceleration
+		$Sprite2D.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		if (velocity.x > 0):
+			velocity.x = velocity.x * -1
+		velocity.x -= acceleration
+		$Sprite2D.flip_h = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x,0.0,0.5)
 
+	if Input.is_action_pressed("ui_select"):
+		if is_on_floor():
+			velocity.y = -jump_force
+	
+	velocity.x = clamp(velocity.x, max_speed*-1, max_speed)
+	
 	move_and_slide()
+		
